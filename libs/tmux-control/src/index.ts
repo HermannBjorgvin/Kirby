74e9b3bf03bf6027df7108c9d7e43602508991e4
Hermann Björgvin Haraldsson <hermann.haraldsson@marel.com>
2026-03-03 18:26:23 +0000

    refactor: remove dead ControlConnection code from tmux-control
    ControlConnection (tmux control mode protocol) was replaced by
    PtySession + TerminalEmulator. Remove the dead code and clean up
    the barrel export.

--- libs/tmux-control/src/index.ts
+++ libs/tmux-control/src/index.ts
@@ -1,12 +1,2 @@
-export {
-  ControlConnection,
-  unescapeOutput,
-  type ControlConnectionState,
-  type OutputEvent,
-  type CommandResponse,
-  type ControlConnectionEvents,
-} from './lib/control-connection.js';
-
 export { PtySession, type PtySessionOptions } from './lib/pty-session.js';
-
 export { TerminalEmulator } from './lib/terminal-emulator.js';
